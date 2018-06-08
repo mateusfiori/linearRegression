fc957fbc1e22654ad42eaa3445e3a90b0ce83841
mateusfiori <mateus_fyory@hotmail.com>
2018-06-08 17:54:39 -0300

    Added IQR implementation for outliers detection

---
+++ .ipynb_checkpoints/outliearNoveltyDetection.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import load_boston
+from scipy.stats import iqr
+import matplotlib.pyplot as plt
+import numpy as np
+
+boston = load_boston()
+
+X = boston.data
+y = boston.target
+
+labelX = []
+colArray = []
+feat = 0
+
+print(X.shape)
+
+while feat < 13:
+    colArray.append(iqr(X[:, feat]))
+    labelX.append(str(feat))
+    print("IQR (coluna {}: {}".format(feat, iqr(X[:, feat])))
+    feat += 1
+
+x1 = np.arange(labelX.__len__())
+
+plt.title('IQR')
+plt.xlabel('Index da coluna')
+plt.ylabel('IQR value')
+plt.plot(x1, colArray)
+plt.xticks(x1, labelX)
+
+plt.show()
+
+
